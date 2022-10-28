5279407d3ccddaf7ee12340d5badd6c3919284f7
Miguel E. Ruiz <meruiz77@gmail.com>
2022-10-28 02:16:57 +0200

    Add Var1, Var2 rules. Use Elixir syntax in functions.

--- lib/logic_elixir.ex
+++ lib/logic_elixir.ex
@@ -3,50 +3,65 @@ defmodule LogicElixir do
   Documentation for `LogicElixir`.
   """
 
-  # TODO add map() as part of t() definition
-  # ? Las variables lógicas las representamos con átomos??
-  @type t :: {:ground, term()} | {t()} | list(t()) | atom()
+  #########
+  # Types #
+  #########
 
-  # el :unmatch sirve para representar el simbolo ⊥
-  @type sigma :: list(tuple()) | :unmatch
+  # TODO add map() as part of t() definition
+  @type t :: {:ground, term()} | {t()} | [t()] | atom()
+
+  # sigma could be either a map to keep the substitutions or
+  # :unmatch atom to represent ⊥ symbol
+  @type sigma :: %{t() => t()} | :unmatch
+
+  #############
+  # Functions #
+  #############
 
   @spec unify(t(), t(), sigma()) :: sigma()
   # [ExTerm] rule
-  def unify({:ground, t}, {:ground, t}, sigma) do
-    sigma
-  end
+  def unify({:ground, t}, {:ground, t}, sigma), do: sigma
 
   # [ExTermFail] Rule
-  def unify({:ground, t1}, {:ground, t2}, _sigma) when t1 != t2 do
-    :unmatch
-  end
+  def unify({:ground, _}, {:ground, _}, _sigma), do: :unmatch
+
+  # [Var1] [Var2] Rules
+  def unify(t1, t2, sigma) when is_atom(t1) or is_atom(t2), do: unify_variable(t1, t2, sigma)
 
   # [Id] Rule
-  def unify(t, t, sigma) do
-    sigma
-  end
+  def unify(t, t, sigma) when is_atom(t), do: sigma
 
-  def unify(t1, t1, sigma) when is_tuple(t1) do
-    sigma
-  end
+  # [Tuple] Rule
+  # TODO not a valid implementation
+  def unify(t, t, sigma) when is_tuple(t), do: sigma
 
-  def unify([], [], sigma) do
-    sigma
-  end
-
-  def unify([head1 | tail1], [head2 | tail2], sigma) do
-    # probably it's not okay
-    unify(tail1, tail2, [{head1, head2} | sigma])
-  end
+  # [List] Rule
+  # TODO not a valid implementation
+  def unify(t, t, sigma) when is_list(t), do: sigma
 
   # [Clash] Rule
-  def unify(_t1, _t2, _sigma) do
-    :unmatch
-  end
+  def unify(_t1, _t2, _sigma), do: :unmatch
 
   # TODO faltan:
-  # Var1
-  # Var2
   # Occurs-check
   # Orient
+
+  #####################
+  # Private Functions #
+  #####################
+
+  @spec unify_variable(t(), t(), sigma()) :: sigma()
+  defp unify_variable(t1, t2, sigma) when is_atom(t1) do
+    case Map.fetch(sigma, t1) do
+      {:ok, subt} -> unify(subt, t2, sigma)
+      :error -> Map.put(sigma, t1, t2)
+    end
+  end
+
+  defp unify_variable(t1, t2, sigma) when is_atom(t2) do
+    case Map.fetch(sigma, t2) do
+      {:ok, subt} -> unify(t1, subt, sigma)
+      :error -> Map.put(sigma, t2, t1)
+    end
+  end
 end
