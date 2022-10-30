1a3b51b443a95205534e40624d0b26face2a49c3
Miguel E. Ruiz <meruiz77@gmail.com>
2022-10-30 14:40:06 +0100

    Fix some rules. Still needs more development

--- lib/logic_elixir.ex
+++ lib/logic_elixir.ex
@@ -18,7 +18,7 @@ defmodule LogicElixir do
   # Guards #
   ##########
 
-  defguard is_tuple_term(t) when is_tuple(t) and (elem(t, 0) != :ground or is_tuple(elem(t, 1)))
+  defguardp is_tuple_term(t) when is_tuple(t) and (elem(t, 0) != :ground or is_tuple(elem(t, 1)))
 
   #############
   # Functions #
@@ -31,18 +31,18 @@ defmodule LogicElixir do
   # [ExTermFail] Rule
   def unify({:ground, _}, {:ground, _}, _sigma), do: :unmatch
 
-  # [Var1] [Var2] Rules
-  def unify(t1, t2, sigma) when is_atom(t1) do
-    case is_logic_var?(t1) do
-      true -> unify_variable(t1, t2, sigma)
-      false -> :unmatch
-    end
-  end
-
   # [Id] Rule
   def unify(t, t, sigma) when is_atom(t) do
     case is_logic_var?(t) do
       true -> sigma
+      false -> :unmatch
+    end
+  end
+
+  # [Var1] [Var2] Rules
+  def unify(t1, t2, sigma) when is_atom(t1) do
+    case is_logic_var?(t1) do
+      true -> unify_variable(t1, t2, sigma)
       false -> :unmatch
     end
   end
@@ -56,14 +56,14 @@ defmodule LogicElixir do
   end
 
   # [Tuple] Rule
-  # TODO not a valid implementation
   def unify(t1, t2, sigma) when is_tuple_term(t1) and is_tuple_term(t2) do
     c1 = components_of(t1)
     c2 = components_of(t2)
+    unify(c1, c2, sigma)
   end
 
   # [List] Rule
-  # TODO check if its correct
+  def unify([], [], sigma), do: sigma
   def unify([h1, t1], [h2, t2], sigma) do
     case unify(h1, h2, sigma) do
       :unmatch -> :unmatch
@@ -81,17 +81,18 @@ defmodule LogicElixir do
   # Private Functions #
   #####################
 
-  # TODO Not only unify/3 but make the possible substitutions
+  # TODO Not only unify/3 but make the possible substitutions (Occurs-check)
   @spec unify_variable(t(), t(), sigma()) :: sigma()
-  defp unify_variable(t1, t2, sigma) when is_atom(t1) do
+  defp unify_variable(t1, t2, sigma) do
     case Map.fetch(sigma, t1) do
       {:ok, subt} -> unify(subt, t2, sigma)
       :error -> Map.put(sigma, t1, t2)
     end
   end
 
-  @spec components_of(tuple()) :: [term()]
-  defp components_of({:ground, t}), do: Tuple.to_list(t)
+  @spec components_of(tuple()) :: [term()] | term()
+  defp components_of({:ground, t}) when is_tuple(t), do: Tuple.to_list(t)
+  defp components_of({:ground, t}), do: t
   defp components_of(t), do: Tuple.to_list(t)
 
   @spec is_logic_var?(atom()) :: boolean()
