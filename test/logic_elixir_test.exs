13fcec5ad7265cc8f27092d72b03fc7e80603d67
Miguel E. Ruiz <meruiz77@gmail.com>
2022-10-31 01:03:02 +0100

    import LogicElixir module in tests

--- test/logic_elixir_test.exs
+++ test/logic_elixir_test.exs
@@ -2,42 +2,46 @@ defmodule LogicElixirTest do
   use ExUnit.Case
   doctest LogicElixir
 
+  import LogicElixir
+
   test "Verifies [ExTerm] rule" do
-    assert LogicElixir.unify({:ground, 3}, {:ground, 3}, %{}) == %{}
-    assert LogicElixir.unify({:ground, :a}, {:ground, :a}, %{}) == %{}
-    assert LogicElixir.unify({:ground, "hello"}, {:ground, "hello"}, %{}) == %{}
-    assert LogicElixir.unify({:ground, true}, {:ground, true}, %{}) == %{}
-    assert LogicElixir.unify({:ground, false}, {:ground, false}, %{}) == %{}
+    assert unify({:ground, 3}, {:ground, 3}, %{}) == %{}
+    assert unify({:ground, :a}, {:ground, :a}, %{}) == %{}
+    assert unify({:ground, "hello"}, {:ground, "hello"}, %{}) == %{}
+    assert unify({:ground, true}, {:ground, true}, %{}) == %{}
+    assert unify({:ground, false}, {:ground, false}, %{}) == %{}
   end
 
   test "Verifies [ExTermFail] rule" do
-    assert LogicElixir.unify({:ground, 3}, {:ground, 5}, %{}) == :unmatch
-    assert LogicElixir.unify({:ground, :a}, {:ground, :b}, %{}) == :unmatch
+    assert unify({:ground, 3}, {:ground, 5}, %{}) == :unmatch
+    assert unify({:ground, :a}, {:ground, :b}, %{}) == :unmatch
   end
 
   test "Verifies [Id] rule" do
-    assert LogicElixir.unify(X, X, %{}) == %{}
-    assert LogicElixir.unify(Y, Y, %{}) == %{}
-    assert LogicElixir.unify(Z, Z, %{}) == %{}
+    assert unify(X, X, %{}) == %{}
+    assert unify(Y, Y, %{}) == %{}
+    assert unify(Z, Z, %{}) == %{}
   end
 
-  test "Verifies [Var1], [Var2] and [Orient] rules" do
-    assert LogicElixir.unify(X, Y, %{}) == %{'Elixir.X': Y}
-    assert LogicElixir.unify(Y, X, %{}) == %{'Elixir.Y': X}
-    assert LogicElixir.unify(X, {:ground, 3}, %{}) == %{'Elixir.X': 3}
-    assert LogicElixir.unify({:ground, 3}, X, %{}) == %{'Elixir.X': 3}
-    # assert LogicElixir.unify({X, X}, {:ground, {5, 5}}, %{}) == %{'Elixir.X': 5}
+  test "Verifies [Var1], [Var2], [Occurs-check] and [Orient] rules" do
+    assert unify(X, Y, %{}) == %{'Elixir.X': Y}
+    assert unify(Y, X, %{}) == %{'Elixir.Y': X}
+    assert unify(X, {:ground, 3}, %{}) == %{'Elixir.X': 3}
+    assert unify({:ground, 3}, X, %{}) == %{'Elixir.X': 3}
+    assert unify({X, X}, {:ground, {5, 5}}, %{}) == %{'Elixir.X': 5}
   end
 
   test "Verifies [List] rule" do
-    assert LogicElixir.unify([], [], %{}) == %{}
-    assert LogicElixir.unify([{:ground, 1}, {:ground, 2}, {:ground, 3}], [{:ground, 1}, {:ground, 2}, {:ground, 3}], %{}) == %{}
-    assert LogicElixir.unify([{:ground, 1}, {:ground, 2}, {:ground, 3}], [X, Y, Z], %{}) == %{'Elixir.X': 1, 'Elixir.Y': 2, 'Elixir.Z': 3}
+    assert unify([], [], %{}) == %{}
+    assert unify([{:ground, 1}, {:ground, 2}, {:ground, 3}], [{:ground, 1}, {:ground, 2}, {:ground, 3}], %{}) == %{}
+    assert unify([{:ground, 1}, {:ground, 2}, {:ground, 3}], [X, Y, Z], %{}) == %{'Elixir.X': 1, 'Elixir.Y': 2, 'Elixir.Z': 3}
+    assert unify([X, X], [{:ground, 5}, {:ground, 5}], %{}) == %{'Elixir.X': 5}
+    assert unify([Y, X, Z], [{:ground, 5}, {Y, {:ground, 3}}, {:ground, :a}], %{}) == %{'Elixir.Y': 5, 'Elixir.X': {5, 3}, 'Elixir.Z': :a}
   end
 
   test "Verifies [Clash] rule" do
-    assert LogicElixir.unify(:a, 3, %{}) == :unmatch
-    assert LogicElixir.unify(:a, :a, %{}) == :unmatch
-    assert LogicElixir.unify({X, Y}, {:ground, 3}, %{}) == :unmatch
+    assert unify(:a, 3, %{}) == :unmatch
+    assert unify(:a, :a, %{}) == :unmatch
+    assert unify({X, Y}, {:ground, 3}, %{}) == :unmatch
   end
 end
