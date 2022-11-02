18635fcd79e36b0cebae4cd6a86e240e799df15d
Miguel E. Ruiz <meruiz77@gmail.com>
2022-11-03 00:27:26 +0100

    Add dialyzer on mix

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule LogicElixir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [flags: ["-Wunmatched_returns", :error_handling, :underspecs]]
     ]
   end
 
