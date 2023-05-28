4c6484d3194f97af304f061852e5502c821e1052
Miguel E. Ruiz <meruiz77@gmail.com>
2023-05-28 22:07:38 +0200

    First workaround on `defpred` operator

--- lib/logic_elixir.ex
+++ lib/logic_elixir.ex
@@ -2,4 +2,58 @@ defmodule LogicElixir do
   @moduledoc """
   Documentation for `LogicElixir`.
   """
+
+  #########
+  # Types #
+  #########
+
+  ##########
+  # Guards #
+  ##########
+
+  ##########
+  # Macros #
+  ##########
+
+  defmacro defpred({name, _metadata, args}) do
+    quote do
+      Module.put_attribute(__MODULE__, :definitions, {unquote(name), unquote(args)})
+    end
+  end
+
+  defmacro __before_compile__(env) do
+    definitions = Module.get_attribute(env.module, :definitions)
+    for {name, args} <- definitions do# |> Enum.group_by(&elem(&1, 0), &elem(&1, 1)) do
+      quote do
+        defcore unquote(name)(X1, X2) do
+          unquote(Enum.at(args, 0)) = X1
+          unquote(Enum.at(args, 1)) = X2
+        end
+      end
+    end
+  end
+
+  defmacro __using__(_params) do
+    quote do
+      import LogicElixir, only: [defpred: 1]
+      import Core
+      Module.register_attribute(__MODULE__, :definitions, accumulate: true)
+      @before_compile LogicElixir
+    end
+  end
+
+  #############
+  # Functions #
+  #############
+
+  ###############################
+  #  Public auxiliar functions  #
+  ###############################
+
+  def to_core(t) when is_tuple(t), do: t
+  def to_core(lit), do: {:ground, lit}
+
+  #####################
+  # Private Functions #
+  #####################
 end
---
+++ lib/logic_template.ex
@@ -0,0 +1,26 @@
+defmodule LogicTemplate do
+  use LogicElixir
+
+  defpred likes(:sussie, :pizza)
+
+  # defpred age(john, 50)
+
+  # defpred age(paul, 30)
+
+  # defpred age(mary, 20)
+
+  # defpred father_of(john, paul)
+
+  # defpred father_of(john, mary)
+
+  # defpred siblings(X, Y) do
+  #   father_of(Z, X)
+  #   father_of(Z, Y)
+  # end
+
+  # defpred append([], Ys, Ys)
+
+  # defpred append([X|Xs], Ys, [X|Zs]) do
+  #   append(Xs, Ys, Zs)
+  # end
+end
