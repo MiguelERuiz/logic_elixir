2358afe188d2626592190d5e70d195566415cd29
Miguel E. Ruiz <meruiz77@gmail.com>
2023-06-28 21:16:09 +0200

    Untrack VarChecker module

--- lib/logic_elixir/var_checker.ex
+++
@@ -1,7 +0,0 @@
-defmodule LogicElixir.VarChecker do
-  def is_logic_variable?({:__aliases__, _metadata, [logic_variable]})
-      when is_atom(logic_variable),
-      do: true
-
-  def is_logic_variable?(_), do: false
-end
